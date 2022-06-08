a6893d10861b9351f37c2336f2b1706b2296c0f7
mizuki-n-2 <mizukizhangze@gmail.com>
2022-06-09 00:14:16 +0900

    update

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/sivchari/gotwtr"
@@ -26,8 +28,36 @@ func main() {
 		log.Fatal(err)
 	}
 	
+	idKeyword := "ゲリラ招待ID:"
+	dateKeyword := "期限:"
+	now := time.Now()
+	datetimeFormat := "2006/01/02 15:04"
+	jst, _ := time.LoadLocation("Asia/Tokyo")
+
 	for _, t := range tsr.Tweets {
+		idIndex := strings.Index(t.Text, idKeyword)
+		if idIndex == -1 {
+			fmt.Println("フォーマットが違う(ゲリラ招待ID)")
+			continue
+		}
+		id := t.Text[idIndex+len(idKeyword):idIndex+len(idKeyword)+8]
+		// TODO: IDで既に送信済みか判定する
+		fmt.Println(id)
+		
+		dateIndex := strings.Index(t.Text, dateKeyword)
+		if dateIndex == -1 {
+			fmt.Println("フォーマットが違う(期限)")
+			continue
+		}
+		datetimeStr := t.Text[dateIndex+len(dateKeyword):dateIndex+len(dateKeyword)+11]
+		datetime, _ := time.ParseInLocation(datetimeFormat, fmt.Sprintf("%d/%s", now.Year(), datetimeStr), jst)
+
+		fmt.Println(now)
+		fmt.Println(datetime)
 		fmt.Println("---")
-		fmt.Println(t.Text)
+		if now.Before(datetime) {
+			// TODO: メッセージを送信
+			fmt.Println(t.Text)
+		}
 	}
 }
